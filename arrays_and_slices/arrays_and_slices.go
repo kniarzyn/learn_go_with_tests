552c4c3e9795110dee80381dec49b7365ea4bcaf
kniarzyn <kniarzyn@gmail.com>
2024-10-28 16:12:48 +0100

    Array and slices part 2

--- arrays_and_slices/arrays_and_slices.go
+++ arrays_and_slices/arrays_and_slices.go
@@ -8,3 +8,25 @@ func Sum(s []int) int {
 	}
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	var r []int
+	for _, s := range slices {
+		r = append(r, Sum(s))
+	}
+	return r
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, s := range slices {
+		el := 0
+		if len(s) > 0 {
+			el = Sum(s[1:])
+		}
+		sums = append(sums, el)
+	}
+
+	return sums
+}
