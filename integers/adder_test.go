4cfb7dba31739ed90d9bc86452293726147f45b0
kniarzyn <kniarzyn@gmail.com>
2025-07-19 20:17:59 +0200

    add integers

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,20 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAdder(t *testing.T) {
+	sum := Add(2, 3)
+
+	assert.Equal(t, 5, sum)
+}
+
+func ExampleAdd() {
+	sum := Add(3, 5)
+	fmt.Println(sum)
+	// Output: 8
 }
